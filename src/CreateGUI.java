8edf0051decd418dee6a3b14466610bcfd786c98
Jacek Rachwalski <jacek.rachwalski@gmail.com>
2023-11-17 22:54:11 +0100

    Initial commit

---
+++ src/CreateGUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class CreateGUI
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setSize(640,480);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setTitle("Okno klasy CreateGUI");
+        frame.setVisible(true);
+    }
+}
